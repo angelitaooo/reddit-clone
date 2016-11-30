dcf7a902c92f893bc46770aae2543d2bfaf2a662
Angela Ordonez <angelitaooo@me.com>
2016-11-29 21:23:27 -0500

    initial components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+class Article{
+  constructor(
+    public title: string,
+    public description: string
+  ){ }
+}
+
+@Component({
+  selector: 'app-sidebar',
+  template: `
+    <div id="sidebar">sidebar will go here</div>
+  `
+})
+export class SidebarComponent{}
+
+@Component({
+  selector: 'app-article',
+  template: `
+    <pre>{{article | json}}</pre>
+  `
+})
+export class ArticleComponent{
+  @Input() article: Object;
+}
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div id="container">
+      <app-sidebar></app-sidebar>
+      <div id="content">
+        <app-article
+          *ngFor="let article of articles"
+          [article]="article"></app-article>
+      </div>
+    </div>
+  `
 })
 export class AppComponent {
-  title = 'app works!';
+  articles: Article[];
+
+  constructor(){
+    this.articles = [
+      new Article('angular 2', 'easiest way to learn'),
+      new Article('react', 'super fun'),
+      new Article('jquery', 'miss you')
+    ];
+  }
 }
