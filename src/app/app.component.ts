e1eeaabcab3edd538a7c173177c54ffdc29e71b5
Angela Ordonez <angelitaooo@me.com>
2016-11-30 18:56:35 -0500

    html article

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,51 @@ export class SidebarComponent{}
 @Component({
   selector: 'app-article',
   template: `
-    <pre>{{article | json}}</pre>
+  <div class="image">
+    <img src="http://placekitten.com/300/300"/>
+  </div>
+  <div class="content">
+    <div class='header'>
+      {{ article.title }}
+    </div>
+    <div class="meta">
+      <span class="ui blue small label">
+        <i class="heart icon"></i>
+        <div class="detail">
+          {{ article.votes }}
+        </div>
+      </span>
+      <span class="ui right floated">
+        <a
+          (click)="upvote()"
+          class="ui small label">
+          <i class="arrow up icon"></i>
+          Upvote
+        </a>
+        <a
+          (click)="downvote()"
+          class="ui small label">
+          <i class="arrow down icon"></i>
+          Downvote
+        </a>
+      </span>
+    </div>
+    <div class="meta date">
+      {{ article.publishedAt | date:'medium' }}
+    </div>
+    <div class="meta description">
+      <p>{{ article.description }}</p>
+    </div>
+    <div class="extra">
+      <a
+        href='#'
+        target='_blank'
+        class='ui right floated button primary'>
+          Read more
+          <i class='right chevron icon'></i>
+      </a>
+    </div>
+  </div>
   `
 })
 export class ArticleComponent{
@@ -28,12 +72,13 @@ export class ArticleComponent{
 @Component({
   selector: 'app-root',
   template: `
-    <div id="container">
+    <div class="ui container">
       <app-sidebar></app-sidebar>
-      <div id="content">
+      <div class="ui divided items">
         <app-article
           *ngFor="let article of articles"
-          [article]="article"></app-article>
+          [article]="article" class="item">
+        </app-article>
       </div>
     </div>
   `
